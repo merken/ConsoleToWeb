5189a9705c65ec953c7d624d26cb075c470fb09e
Maarten <merken.maarten@gmail.com>
2021-04-01 18:03:24 +0200

    Using dependencyInjection

--- src/ConsoleToWebTemplate/Program.cs
+++ src/ConsoleToWebTemplate/Program.cs
@@ -3,9 +3,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using FuncR;
+using System;
+using System.Linq;
 
 namespace ConsoleToWebTemplate
 {
+    interface IFooService
+    {
+        string Foo(int numberOfFoos);
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -16,8 +24,20 @@ namespace ConsoleToWebTemplate
         private static bool IsRootRequest(HttpRequest request)
             => request.Path.Value.Equals("/");
 
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScopedFunction<IFooService>
+                (nameof(IFooService.Foo))
+                .Runs<int, string>(numberOfFoos =>
+                {
+                    var foos = Enumerable.Range(1, numberOfFoos).Select(n => "Foo");
+                    return $"{String.Join(", ", foos)}";
+                });
+        }
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureServices(ConfigureServices)
                 .ConfigureWebHost(
                     webHost => webHost
                         .UseKestrel(kestrelOptions => { kestrelOptions.ListenAnyIP(5005); }) // HTTP
@@ -27,8 +47,9 @@ namespace ConsoleToWebTemplate
                                 {
                                     if (!IsRootRequest(context.Request))
                                         return; // Any other request then '/'
-
-                                    await context.Response.WriteAsync("Hello World!");
+                                        
+                                    var fooService = context.RequestServices.GetRequiredService<IFooService>();
+                                    await context.Response.WriteAsync(fooService.Foo(4));
                                 }
                             )));
     }
