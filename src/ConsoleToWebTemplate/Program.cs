e1f78a66c73ad27c457b9078c34d49f253102a1a
Maarten <merken.maarten@gmail.com>
2021-04-01 17:55:12 +0200

    Hello World!

--- src/ConsoleToWebTemplate/Program.cs
+++ src/ConsoleToWebTemplate/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ConsoleToWebTemplate
 {
@@ -6,7 +10,26 @@ namespace ConsoleToWebTemplate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        private static bool IsRootRequest(HttpRequest request)
+            => request.Path.Value.Equals("/");
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHost(
+                    webHost => webHost
+                        .UseKestrel(kestrelOptions => { kestrelOptions.ListenAnyIP(5005); }) // HTTP
+                        .Configure(app => app
+                            .Run(
+                                async context =>
+                                {
+                                    if (!IsRootRequest(context.Request))
+                                        return; // Any other request then '/'
+
+                                    await context.Response.WriteAsync("Hello World!");
+                                }
+                            )));
     }
 }
