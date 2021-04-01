c6ec611616ce2c54e56d214aa83bffb4d8555319
Maarten <merken.maarten@gmail.com>
2021-04-01 17:48:50 +0200

    Initial commit

---
+++ src/ConsoleToWebTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleToWebTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
